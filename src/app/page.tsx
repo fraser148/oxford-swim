73431ca997b23fd33f87bf224e9fdf5341bb7d21
Fraser Rennie <fjrennie1@outlook.com>
2023-05-23 11:52:58 +0100

    allow updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,7 +108,7 @@ async function getData() {
                 client_id: clientID,
                 client_secret: clientSecret
             },
-            // cache: 'no-store',
+            cache: 'no-store',
         });
         const data = await res.json();
         let temp_dumps = data.items
