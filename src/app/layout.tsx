0c99590b9deb755f2352a1f8e74dac37d49bba9d
Fraser Rennie <fjrennie1@outlook.com>
2023-05-26 21:01:14 +0100

    add ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
+
 export const metadata = {
   title: 'Oxford Swim Discharge Watch',
   description: 'Check the last time there was a discharge upstream of your location.',
@@ -16,6 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5670628547065791"
+        crossOrigin="anonymous"></script>
+      </head>
       <body className={inter.className}>{children}<Analytics /></body>
     </html>
   )
