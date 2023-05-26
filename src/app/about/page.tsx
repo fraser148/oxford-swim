d68ef8ebc21be8e8cef151da6a2b2e4b486d89ff
Fraser Rennie <fjrennie1@outlook.com>
2023-05-26 15:11:42 +0100

    swims

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
                     <ArrowUturnLeftIcon className="h-8 w-8  text-gray-800 rounded-full hover:bg-blue-500 hover:text-white p-1 transition cursor-pointer"/>
                 </Link>
                 <h1 className="text-xl font-bold text-gray-800">About:</h1>
-                <h1 className="text-6xl md:text-8xl pb-2 mt-6 font-bold text-center text-transparent bg-clip-text bg-gradient-to-l from-lime-700 to-yellow-900 overflow-auto">372,533</h1>
+                <h1 className="text-6xl md:text-8xl pb-2 mt-6 font-bold text-center text-transparent bg-clip-text bg-gradient-to-l from-lime-700 to-yellow-900">372,533</h1>
                 <h4 className="font-light text-center">dumps of <span className="font-semibold">raw sewage</span> into our rivers in 2021 </h4>
                 <h1 className="text-6xl md:text-8xl pb-2 mt-12 font-bold text-center text-transparent bg-clip-text bg-gradient-to-l from-lime-700 to-yellow-900">2.75m+</h1>
                 <h4 className="font-light text-center">hours of <span className="font-semibold">raw sewage</span> discharge.</h4>
@@ -33,14 +33,14 @@ export default function Page() {
                     Our rivers are being polluted by sewage discharges which has long been hidden from public view. Thanks to work from activists, such as Ash Smith, working to expose this issue, Thames Water and other water companies are now legally required to publish this data under the Environment Act. This website is an attempt to make this data more accessible to the public.
                 </p>
                 <hr className="my-4"/>
-                <p className="my-2 font-light">
+                {/* <p className="my-2 font-light">
                     This website was created and is maintained by <a className="text-blue-500 visited:text-purple-700 underline" href="https://github.com/fraser148">Fraser Rennie</a>.
-                </p>
+                </p> */}
                 <p className="my-2 font-light">
                     Have an issue or want to contibute? <a className="text-blue-500 visited:text-purple-700 underline" href="https://github.com/fraser148/oxford-swim">Visit the GitHub repo</a>.
                 </p>
                 <p className="my-2 font-light">
-                    Contact me at <a className="text-blue-500 visited:text-purple-700 underline" href="mailto:oxfordswim@oxtickets.co.uk">oxfordswim@oxtickets.co.uk</a> to request a feature or new location.
+                    Contact me at <a className="text-blue-500 hover:text-blue-700 visited:text-purple-700 underline transition" href="mailto:oxfordswim@oxtickets.co.uk">oxfordswim@oxtickets.co.uk</a> to request a feature or new location.
                 </p>
             </div>
             <div className="flex max-w-md w-full rounded-md shadow-lg overflow-hidden">
